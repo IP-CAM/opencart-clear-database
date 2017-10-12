ed6eee44676f6fa0a1ed5da23786dbba0fe9bf7a
David Wright <dave@coderstudios.com>
2017-10-12 22:35:52 +0100

    Truncate SQL for 3.0.2.1b Opencart

---
+++ 3.0.2.1b/oc-truncate-3-0-2-1b.sql
@@ -0,0 +1,66 @@
+/* Replace _prefix_ with your table prefix or remove completely if no prefix is used */
+
+truncate table _prefix_attribute;
+truncate table _prefix_attribute_description;
+truncate table _prefix_attribute_group;
+truncate table _prefix_attribute_group_description;
+truncate table _prefix_banner;
+truncate table _prefix_banner_image_description;
+truncate table _prefix_category;
+truncate table _prefix_category_description;
+truncate table _prefix_category_path;
+truncate table _prefix_category_to_store;
+truncate table _prefix_coupon;
+truncate table _prefix_manufacturer;
+truncate table _prefix_manufacturer_to_store;
+truncate table _prefix_option;
+truncate table _prefix_option_description;
+truncate table _prefix_option_value;
+truncate table _prefix_option_value_description;
+truncate table _prefix_product;
+truncate table _prefix_product_attribute;
+truncate table _prefix_product_description;
+truncate table _prefix_product_discount;
+truncate table _prefix_product_image;
+truncate table _prefix_product_option;
+truncate table _prefix_product_option_value;
+truncate table _prefix_product_related;
+truncate table _prefix_product_reward;
+truncate table _prefix_product_special;
+truncate table _prefix_product_to_category;
+truncate table _prefix_product_to_download;
+truncate table _prefix_product_to_store;
+truncate table _prefix_seo_url;
+truncate table _prefix_session;
+truncate table _prefix_voucher_theme;
+truncate table _prefix_voucher_theme_description;
+
+/* Optional tables for a totally clear database */
+
+truncate table _prefix_customer_group;
+truncate table _prefix_customer_group_description;
+truncate table _prefix_extension;
+truncate table _prefix_geo_zone;
+truncate table _prefix_information;
+truncate table _prefix_information_description;
+truncate table _prefix_information_to_store;
+truncate table _prefix_language;
+truncate table _prefix_layout;
+truncate table _prefix_layout_module;
+truncate table _prefix_layout_route;
+truncate table _prefix_length_class;
+truncate table _prefix_length_class_description;
+truncate table _prefix_module;
+truncate table _prefix_order_status;
+truncate table _prefix_return_action;
+truncate table _prefix_return_reason;
+truncate table _prefix_return_status;
+truncate table _prefix_shipping_courier;
+truncate table _prefix_stock_status;
+truncate table _prefix_tax_class;
+truncate table _prefix_tax_rate;
+truncate table _prefix_tax_rate_to_customer_group;
+truncate table _prefix_tax_rule;
+truncate table _prefix_weight_class;
+truncate table _prefix_weight_class_description;
+truncate table _prefix_zone_to_geo_zone;
